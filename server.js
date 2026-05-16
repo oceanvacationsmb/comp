2786b170c0997c8569b258bcc9521b29c5533746
oceanvacationsmb <oceanvacationsmb@gmail.com>
2026-05-16 13:06:32 -0400

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,21 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "https://oceanvacationsmb.github.io",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: [
+    "Content-Type",
+    "x-admin-password"
+  ]
+}));
+
+app.options("*", cors());
+
 app.use(express.json({ limit: "25mb" }));
 app.use(express.urlencoded({ extended: true, limit: "25mb" }));
 app.use(fileUpload());
